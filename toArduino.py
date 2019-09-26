ac1405b73472c320fab944c363468c4a084430dd
henrique-temponi <tanzenheavely@gmail.com>
2019-09-25 23:38:34 -0300

    finished file in hex to arduino

--- toArduino.py
+++ toArduino.py
@@ -1,6 +1,7 @@
 # fazer decodificador para o arduino usando python 
 # primeiro ler um arquivo com as instrucoes de alto nivel e depois interpretar para o arduino
 arquivo = "textoula.txt"
+saida = "output.txt"
 a = 0
 b = 0
 error = -1
@@ -11,6 +12,7 @@ def debugger(**args):
 
 
 file = open(arquivo, "r")
+outfile = open(saida, "w+")
 inicio = file.readline()
 
 # print(inicio.strip())
@@ -36,18 +38,35 @@ def atribuir_valor(Variavel=error, Valor=-1):
         b = Valor
 
 
-def pegar_Valor(line):
+def pegar_valor(line):
     line = formatar(line)
 
     variavel = line[0]
-    
-    valor = line[len(line) - 2]
-    valor = int(valor, 16)
+
+    valor = line[2:len(line)-1]
 
     print(variavel, type(variavel))
     print(valor, type(valor))
 
     atribuir_valor(Variavel=variavel, Valor=valor)
+
+
+def imprimir_saida(x):
+    outfile.write(str(a))
+    outfile.write(str(b))
+    outfile.write(x)
+    outfile.write("\n")
+
+
+def valor_logico(line):
+    print(formatar(line), end=" tem valor: ")
+    if "um" in line:
+        print("C")
+        imprimir_saida("C")
+
+    else:
+        print(3)
+        imprimir_saida("3")
 
 
 def operacao_or(line, flag=False):
@@ -58,33 +77,64 @@ def operacao_or(line, flag=False):
     else:
         if "An" in line:
             print(8, end="\n")
+            imprimir_saida("8")
 
         elif "Bn" in line:
             print("D", end="\n")
+            imprimir_saida("D")
 
         else:
             print("E", end="\n")
+            imprimir_saida("E")
 
 
 def operacao_xor(line, flag=False):
 
     if flag:
         print(9)  # codigo para or negada
+        imprimir_saida("9")
 
     else:
         print(6)
+        imprimir_saida("6")
 
 
 def operacao_and(line):
 
     if "An" in line:
         print(2, end="\n")
+        imprimir_saida("2")
 
     elif "Bn" in line:
         print(7, end="\n")
+        imprimir_saida("7")
 
     else:
         print("B", end="\n")
+        imprimir_saida("B")
+
+
+def operacao_a(line):
+
+    if 'An' in line:
+        print(0)
+        imprimir_saida("0")
+
+    else:
+        print("F")
+        imprimir_saida("F")
+
+
+def operacao_b(line):
+
+    if 'Bn' in line:
+        print(5)
+        imprimir_saida("5")
+
+    else:
+        print("A")
+        imprimir_saida("A")
+
 
 
 def selecionar_operacao(line, flag=False):
@@ -100,8 +150,17 @@ def selecionar_operacao(line, flag=False):
     elif 'e' in line:
         print(formatar(line), end=" tem valor: ")
         print(4)
+        imprimir_saida("4")
 
-    elif 'A' in line and 'B' in line:
+    elif "B" not in line:
+        print(formatar(line), end=" tem valor: ")
+        operacao_a(line)
+
+    elif "A" not in line:
+        print(formatar(line), end=" tem valor: ")
+        operacao_b(line)
+
+    else:
         print(formatar(line), end=" tem valor: ")
         operacao_and(line)
 
@@ -110,10 +169,16 @@ for x in file:
 
     # print(x)
 
+    if "END." in x:
+        continue
+
     if '=' in x:
 
         print("Atribuir")
-        pegar_Valor(x)
+        pegar_valor(x)
+
+    elif "L" in x:
+        valor_logico(x)
 
     elif 'n' == x[0]:
         selecionar_operacao(x, True)
@@ -122,5 +187,7 @@ for x in file:
         selecionar_operacao(x)
 
 
+file.close()
+outfile.close()
 print(a)
 print(b)
