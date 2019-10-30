6390384fd54bb3531cc5ff6d257b7cb4be33b139
henrique-temponi <tanzenheavely@gmail.com>
2019-10-30 12:34:22 -0300

    Acaba a lista 2

--- AC II/Lista 2/Q17.asm
+++ AC II/Lista 2/Q17.asm
@@ -5,3 +5,56 @@
 # e a quantidade de elementos desse vetor dadas pelo segundo argumento.
 # Cada elemento do vetor é um elemento da série:y[i] = 2i –1 se i for par;y[i] = i se i for impar.
 # O valor retornado será a soma de todos os elementos de y[].
+#
+#
+
+.text
+.globl main
+
+main:
+	addi $a0, $zero, 0x1001		#
+	sll $a0, $a0, 16		#
+	
+	addi $a1, $zero, 4		#
+	
+	jal func			# chamar funcao
+	
+	sw $v0, 0($v1)			# adicionar o somatorio retornado da funcao ao endereco retornado
+	
+	j end
+
+
+	func:
+	
+	add $t1, $zero, $zero		#
+	
+	do:
+	
+		and $t0, $a1, 1		# ( 0 & 1 ) = par / ( 1 & 1 ) = impar
+		beq $t0, $zero, par	#
+		
+			sw $a1, 0($a0)		# quardar o valor de i ( se impar = a[i] = i
+			add $t1, $t1, $a1	# adicionar no somatorio
+			j fim		 	# pular para o fim da repeticao
+		
+		par:
+		
+			sll $t0, $a1, 1		# 2i
+			addi $t0, $t0, -1	# 2i - 1
+		
+			sw $t0, 0($a0)		# quardar o valor de i ( se par a[i] = 2i - 1 )
+			add $t1, $t1, $t0	# adicionar no somatorio
+		
+		fim:
+		
+		
+		addi $a0, $a0, 4	# ajustar valor do endereco
+		addi $a1, $a1 -1	# subtrair a quantidade total
+		bne $a1, $zero, do	# comparar a zero como parada da repeticao
+		
+	add $v0, $zero, $t1		#
+	add $v1, $zero, $a0
+	jr $ra
+		
+	end:
+	
--- AC II/Lista 2/Q18.asm
+++ AC II/Lista 2/Q18.asm
@@ -4,3 +4,50 @@
 # Obs.: Caso você não tenha visto a utilização de handlers e a leitura de valores pelo teclado,
 #  os dois números deverão ser lidos da primeira e segunda posição livre da memória.
 #  O resultado será escrito na terceira posição livre da memória e o programa irá executar apenas uma vez.
+#
+#
+#
+
+
+.text
+.globl main
+
+main:
+
+	addi $t0, $zero, 0x1001		#
+	sll $t0, $t0, 16		# pegar endereco
+	
+	lw $a0, 0($t0)			# carregar o primeiro valor
+	lw $a1, 4($t0)			# carregar o segundo valor
+	
+	jal func
+	
+	sw $v0, 8($t0)			#
+	j fim
+	
+	
+	func:
+	
+		addi $t1, $zero, 1	#
+	
+	do:
+	
+		mult $t1, $a0		#
+		mflo $t1		#
+		
+		
+		addi $a1, $a1, -1	#
+		
+		bne $a1, $zero, do
+		
+	add $v0, $zero, $t1	#	
+	jr $ra
+		
+
+	fim:
+	
+.data
+
+	a: .word 2
+	b: .word 5
+	c: .word -1
