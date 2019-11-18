5fd6399e74fed328fc9877da74c6e6f7a60e5462
henrique-temponi <tanzenheavely@gmail.com>
2019-11-18 18:49:29 -0300

    Acaba o arquivo func2

--- Testes/Assembly/Funcao/func2.asm
+++ Testes/Assembly/Funcao/func2.asm
@@ -1,16 +1,32 @@
-#funcao para chamar outra funcao dentro dela
+#int func1 (int x, int y, int dados){
+#	
+#	int soma = x + y;
+#	
+#	return func2 (soma, dados)
+#}
+#
+#int func2 (int x, int dados) {
+#	
+#	int endereco = dados
+#	array[endereco] = x
+#	return array[endereco]++
+#}
 
 .text
 
 .globl main
 
 main:
+	addi $sp, $sp, -4		# reservar espaco na pilha para o t0
 	
 	addi $t0, $zero, 0x1001		#
-	sll $t0, $t0, 16		#
+	sll $t0, $t0, 16		# Pegar o endereco inicial
+	
+	sw $t0, 4($sp)			# quardar o valor de t0 na pilha
 	
 	lw $a0, 0($t0)			#
 	lw $a1, 4($t0)			#
+	add $a2, $zero, $t0		#
 	
 	jal func
 	
@@ -18,9 +34,48 @@ main:
 	j fim		
 	
 func:
-	add $v0, $a0, $a1
-	jr $ra
+	addi $sp, $sp, -4		# reservar espaco na pilha para o ra
+	sw $ra, 4($sp)			# quardar retorno na pilha
+	
+	add $s0, $a0, $a1		# soma = x + y
+	
+	addi $sp, $sp, -12		# reservar espaco na pilha para o x e y
+	#OBS: e' bom ja reservar tudo na pilha de uma vez? ou aos poucos
+	
+	sw $a0, 4($sp)			# quardar o x
+	sw $a1, 8($sp)			# quardar o y
+	sw $s0, 12($sp)			# quardar o soma ($s0)
+	
+	add $a0, $zero, $s0		# colocar soma no priemiro param
+	add $a1, $zero, $a2		# colocar dados no segundo param
+	
+	jal func2
 
+	lw $a0, 4($sp)			# recuperar valores
+	lw $a1, 8($sp)			#
+	lw $s0, 12($sp)			#
+	lw $ra, 16($sp)			#
+	lw $t0, 20($sp)			#
+	
+	addi $sp, $sp, 20		# resetar pilha
+	
+	add $v0, $zero, $v0		# 
+	jr $ra				#
+
+
+func2:
+	
+	add $s0, $zero, $a2		# pegar o endereco e colocar em s0
+	
+	sw $a0, 8($s0)			# quardar a1 na memoria (soma)
+	
+	addi $t0, $a0, 1		# pegar a1(soma) e adicionar 1
+	add $v0, $zero, $t0		# colocar o valor de retorno em v0
+	
+#	addi $sp, $sp, -4		# reservar espaco para o retorno
+		
+	jr $ra
+	
 fim:
 
 .data
