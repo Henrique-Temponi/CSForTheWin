7280a9bbe87194b8ddb84af75d4794d4c3c060e5
henrique-temponi <tanzenheavely@gmail.com>
2019-11-18 17:10:31 -0300

    adiciona arquivos de test

---
+++ Testes/Assembly/Funcao/func2.asm
@@ -0,0 +1,28 @@
+#funcao para chamar outra funcao dentro dela
+
+.text
+
+.globl main
+
+main:
+	
+	addi $t0, $zero, 0x1001		#
+	sll $t0, $t0, 16		#
+	
+	lw $a0, 0($t0)			#
+	lw $a1, 4($t0)			#
+	
+	jal func
+	
+	sw $v0, 8($t0)			#
+	j fim		
+	
+func:
+	add $v0, $a0, $a1
+	jr $ra
+
+fim:
+
+.data
+	x: .word 10
+	y: .word 10
