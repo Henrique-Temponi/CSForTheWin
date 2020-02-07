5511752a739cde5dddbc95870d39d8fb4a25e101
henrique-temponi <tanzenheavely@gmail.com>
2020-02-07 11:35:14 -0300

    implementa funcao de trocas de janelas

--- Grafos/JavaFxWelcome.java
+++ Grafos/JavaFxWelcome.java
@@ -1,22 +1,44 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Menu;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.scene.paint.Color;
-import java.util.concurrent.TimeUnit;
  
-public class HelloWorld extends Application {
+public class JavaFxWelcome extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("JavaFX Welcome");
-		
-		primaryStage.show();
+        
+        Button butaoParaTest = new Button("Test!"); 
+        Button butaoParaOutra = new Button("TEST");
+        Button pagina2 = new Button("pagina 2");
+        Button pagina1 = new Button("pagina 1");
+
+        VBox boxPagina1 = new VBox(20.0, butaoParaTest, pagina2);
+        VBox boxPagina2 = new VBox(20.0, butaoParaOutra, pagina1);
+
+        Scene rootPagina1 = new Scene(boxPagina1);
+        Scene rootPagina2 = new Scene(boxPagina2);
+
+        pagina1.setOnAction(e -> {
+            primaryStage.setScene(rootPagina1);
+            primaryStage.show();
+        });
+        pagina2.setOnAction(e ->{
+            primaryStage.setScene(rootPagina2);
+            primaryStage.show();
+        });
+
+        butaoParaTest.setOnAction(e -> System.out.println("ok"));
+
+
+        primaryStage.setScene(rootPagina1);
+        primaryStage.show();
     }
- public static void main(String[] args) {
+
+    public static void main(String[] args) {
         launch(args);
     }
 }
